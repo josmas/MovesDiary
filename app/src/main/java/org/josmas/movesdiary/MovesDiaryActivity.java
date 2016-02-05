3492596aaab24f398d45cb7fc7e5201036cd7d26
josmas <josmasflores@gmail.com>
2016-02-05 14:49:22 +0000

    changes floating button and extracts some strings

--- app/src/main/java/org/josmas/movesdiary/MovesDiaryActivity.java
+++ app/src/main/java/org/josmas/movesdiary/MovesDiaryActivity.java
@@ -22,7 +22,7 @@ public class MovesDiaryActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, R.string.add_entry, Snackbar.LENGTH_SHORT)
                         .setAction("Action", null).show();
             }
         });
